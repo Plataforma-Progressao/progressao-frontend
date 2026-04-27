ab0193bcb7dd755804c52d907c10d59acc845528
LuizaBB <luizabezerrabastos@gmail.com>
2026-04-26 23:47:01 -0300

    Ajustes configurações-home page

--- src/app/features/configuracoes/pages / configuracoes-home/configuracoes-home.page.ts
+++ src/app/features/configuracoes/pages / configuracoes-home/configuracoes-home.page.ts
@@ -5,7 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-configuracoes-home',
-  standalone: true, // Padrão recomendado no Angular moderno
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './configuracoes-home.page.html',
   styleUrls: ['./configuracoes-home.page.scss']
@@ -18,7 +18,6 @@ export class ConfiguracoesHomePage implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // Inicializa o formulário com os campos que temos no HTML
     this.configForm = this.fb.group({
       nome: [''],
       email: [''],
@@ -30,7 +29,6 @@ export class ConfiguracoesHomePage implements OnInit {
   }
 
   salvarAlteracoes(): void {
-    // Aqui no futuro você vai conectar com o back-end
     console.log('Tentando salvar as alterações...', this.configForm.value);
   }
 }
